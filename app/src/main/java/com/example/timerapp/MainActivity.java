94b10dcedba290b3c47eb80079221e575f7bae73
jmateo <justin_mateo@sfu.ca>
2022-12-27 14:37:05 -0800

    small changes

--- app/src/main/java/com/example/timerapp/MainActivity.java
+++ app/src/main/java/com/example/timerapp/MainActivity.java
@@ -6,10 +6,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button btnStart;
+    private EditText minTimerText;
+    private EditText secTimerText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,11 +20,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         btnStart =  (Button) findViewById(R.id.start);
+        minTimerText = (EditText) findViewById(R.id.minTimer);
+        secTimerText = (EditText) findViewById(R.id.secTimer);
 
         btnStart.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, PrepareActivity.class);
+                int secs = Integer.parseInt(secTimerText.getText().toString());
+                int mins = Integer.parseInt(minTimerText.getText().toString());
+                intent.putExtra("seconds", secs);
+                intent.putExtra("minutes", mins);
                 startActivity(intent);
             }
         });
--- app/src/main/java/com/example/timerapp/PrepareActivity.java
+++ app/src/main/java/com/example/timerapp/PrepareActivity.java
@@ -2,9 +2,9 @@ package com.example.timerapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.CountDownTimer;
-//import android.support.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -12,28 +12,30 @@ import android.widget.TextView;
 import java.util.Locale;
 
 public class PrepareActivity extends AppCompatActivity {
-    private static final long START_TIME_IN_MILLIS = 5000;
+    private static final long START_TIME_IN_MILLIS_DEFAULT = 5000;
 
     private TextView mTextViewCountDown;
     private Button mButtonStartPause;
-    private Button mButtonReset;
 
     private CountDownTimer mCountDownTimer;
 
     private boolean mTimerRunning;
 
-    private long mTimeLeftInMillis = START_TIME_IN_MILLIS;
+    private long mTimeLeftInMillis = START_TIME_IN_MILLIS_DEFAULT;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_prepare);
+        Intent mIntent = getIntent();
+        mTimeLeftInMillis = 1000 * mIntent.getIntExtra("seconds", 0);
+        mTimeLeftInMillis += 60 * 1000 * mIntent.getIntExtra("minutes", 0);
+
 
         mTextViewCountDown = findViewById(R.id.text_view_countdown);
 
         mButtonStartPause = findViewById(R.id.button_start_pause);
-//        mButtonReset = findViewById(R.id.button_reset);
 
         mButtonStartPause.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -45,13 +47,6 @@ public class PrepareActivity extends AppCompatActivity {
                 }
             }
         });
-
-//        mButtonReset.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                resetTimer();
-//            }
-//        });
 
         updateCountDownText();
     }
@@ -68,28 +63,18 @@ public class PrepareActivity extends AppCompatActivity {
                 mTimerRunning = false;
                 mButtonStartPause.setText("Start");
                 mButtonStartPause.setVisibility(View.INVISIBLE);
-//                mButtonReset.setVisibility(View.VISIBLE);
             }
         }.start();
 
         mTimerRunning = true;
         mButtonStartPause.setText("pause");
-//        mButtonReset.setVisibility(View.INVISIBLE);
     }
 
     private void pauseTimer() {
         mCountDownTimer.cancel();
         mTimerRunning = false;
         mButtonStartPause.setText("Start");
-//        mButtonReset.setVisibility(View.VISIBLE);
     }
-
-//    private void resetTimer() {
-//        mTimeLeftInMillis = START_TIME_IN_MILLIS;
-//        updateCountDownText();
-//        mButtonReset.setVisibility(View.INVISIBLE);
-//        mButtonStartPause.setVisibility(View.VISIBLE);
-//    }
 
     private void updateCountDownText() {
         int minutes = (int) (mTimeLeftInMillis / 1000) / 60;
