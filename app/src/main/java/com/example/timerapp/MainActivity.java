60eb733e2116cbd45c5ca1a4f262a5a8b1108076
jmateo <justin_mateo@sfu.ca>
2022-12-27 21:42:38 -0800

    created sets implementation, loops

--- app/src/main/java/com/example/timerapp/MainActivity.java
+++ app/src/main/java/com/example/timerapp/MainActivity.java
@@ -7,23 +7,31 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button btnStart , btnQuickStart;
+    private Button btnStart , btnQuickStart, btnMinus, btnPlus;
+    private TextView mNumSets;
     private EditText secondInput;
     private EditText minuteInput;
     private EditText second2Input;
     private EditText minute2Input;
     boolean secondsCheck = false;
+    private int numSets;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        numSets = 1;
+
         btnStart =  (Button) findViewById(R.id.start);
         btnQuickStart =  (Button) findViewById(R.id.quickStart);
+        btnMinus = (Button) findViewById(R.id.minus);
+        btnPlus = (Button) findViewById(R.id.plus);
+        mNumSets = (TextView) findViewById(R.id.text_numSets);
 
         secondInput = (EditText) findViewById(R.id.seconds);
         minuteInput = (EditText) findViewById(R.id.minutes);
@@ -34,14 +42,24 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, PrepareActivity.class);
-                int mins = Integer.parseInt(minuteInput.getText().toString());
-                int secs = Integer.parseInt(secondInput.getText().toString());
-                int mins2 = Integer.parseInt(minute2Input.getText().toString());
-                int secs2 = Integer.parseInt(second2Input.getText().toString());
-                intent.putExtra("minutes", mins);
-                intent.putExtra("seconds", secs);
-                intent.putExtra("minutes2", mins2);
-                intent.putExtra("seconds2", secs2);
+                if (!minuteInput.getText().toString().matches("")){
+                    int mins = Integer.parseInt(minuteInput.getText().toString());
+                    intent.putExtra("minutes", mins);
+                }
+                if (!secondInput.getText().toString().matches("")){
+                    int secs = Integer.parseInt(secondInput.getText().toString());
+                    intent.putExtra("seconds", secs);
+                }
+                if (!minute2Input.getText().toString().matches("")){
+                    int mins2 = Integer.parseInt(minute2Input.getText().toString());
+                    intent.putExtra("minutes2", mins2);
+
+                }
+                if (!second2Input.getText().toString().matches("")){
+                    int secs2 = Integer.parseInt(second2Input.getText().toString());
+                    intent.putExtra("seconds2", secs2);
+                }
+                intent.putExtra("numSets", numSets);
                 startActivity(intent);
             }
         });
@@ -53,16 +71,43 @@ public class MainActivity extends AppCompatActivity {
                 secondsCheck = CheckSeconds();
                 if (secondsCheck){
                     Intent intent = new Intent(MainActivity.this, PrepareActivity.class);
-                    int mins = Integer.parseInt(minuteInput.getText().toString());
-                    int secs = Integer.parseInt(secondInput.getText().toString());
-                    int mins2 = Integer.parseInt(minute2Input.getText().toString());
-                    int secs2 = Integer.parseInt(second2Input.getText().toString());
-                    intent.putExtra("minutes", mins);
-                    intent.putExtra("seconds", secs);
-                    intent.putExtra("minutes2", mins2);
-                    intent.putExtra("seconds2", secs2);
+                    if (!minuteInput.getText().toString().matches("")){
+                        int mins = Integer.parseInt(minuteInput.getText().toString());
+                        intent.putExtra("minutes", mins);
+                    }
+                    if (!secondInput.getText().toString().matches("")){
+                        int secs = Integer.parseInt(secondInput.getText().toString());
+                        intent.putExtra("seconds", secs);
+                    }
+                    if (!minute2Input.getText().toString().matches("")){
+                        int mins2 = Integer.parseInt(minute2Input.getText().toString());
+                        intent.putExtra("minutes2", mins2);
+
+                    }
+                    if (!second2Input.getText().toString().matches("")){
+                        int secs2 = Integer.parseInt(second2Input.getText().toString());
+                        intent.putExtra("seconds2", secs2);
+                    }
+                    intent.putExtra("numSets", numSets);
                     startActivity(intent);
                 }
+            }
+        });
+
+        btnMinus.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                if(numSets > 1){
+                    numSets--;
+                }
+                mNumSets.setText(String.format("%d", numSets));
+            }
+        });
+
+        btnPlus.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                mNumSets.setText(String.format("%d", ++numSets));
             }
         });
 
--- app/src/main/java/com/example/timerapp/PrepareActivity.java
+++ app/src/main/java/com/example/timerapp/PrepareActivity.java
@@ -11,11 +11,14 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import java.util.Locale;
+import android.widget.Toast;
+
 
 public class PrepareActivity extends AppCompatActivity {
     private static final long START_TIME_IN_MILLIS_DEFAULT = 5000;
 
     private TextView mTextViewCountDown;
+    private TextView mTextTitle;
     private Button mButtonStartPause;
     private Button mButtonReset;
     private Button mButtonMainMenu;
@@ -23,19 +26,25 @@ public class PrepareActivity extends AppCompatActivity {
     private CountDownTimer mCountDownTimer;
 
     private boolean mTimerRunning;
+    private boolean mResting;
+    private int mNumSets, setsSoFar;
 
     private long mTimeLeftInMillis = START_TIME_IN_MILLIS_DEFAULT;
-    private long mTimeLeftRestInMillis = 0;
+
+    private Intent mIntent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_prepare);
-        Intent mIntent = getIntent();
-        mTimeLeftInMillis = 1000 * (60 * mIntent.getIntExtra("minutes", 0) + mIntent.getIntExtra("seconds", 0));
-        mTimeLeftRestInMillis = 1000 * (60 * mIntent.getIntExtra("minutes2", 0) + mIntent.getIntExtra("seconds2", 0));
+        mResting = false;
+        mIntent = getIntent();
+        mTimeLeftInMillis = 1000 * (60 * (long) mIntent.getIntExtra("minutes", 0) + mIntent.getIntExtra("seconds", 0) + 1);
+        mNumSets = mIntent.getIntExtra("numSets", 1);
+        Toast.makeText(this,String.valueOf(mNumSets),Toast.LENGTH_LONG).show();
+        setsSoFar = 1;
 
         mTextViewCountDown = findViewById(R.id.text_view_countdown);
+        mTextTitle = findViewById(R.id.text_title);
 
         mButtonStartPause = findViewById(R.id.button_start_pause);
         mButtonMainMenu = findViewById(R.id.button_back_to_main_menu);
@@ -60,10 +69,16 @@ public class PrepareActivity extends AppCompatActivity {
                 }
             }
         });
-
         updateCountDownText();
     }
     private void startTimer() {
+        if (mResting){
+            mTextTitle.setText("Rest!");
+        }
+        else{
+            mTextTitle.setText("Work!");
+        }
+
         mCountDownTimer = new CountDownTimer(mTimeLeftInMillis, 100) {
             @Override
             public void onTick(long millisUntilFinished) {
@@ -74,6 +89,29 @@ public class PrepareActivity extends AppCompatActivity {
             @Override
             public void onFinish() {
                 mTimerRunning = false;
+                if (!mResting){
+                    if (setsSoFar != mNumSets){
+                        mTimeLeftInMillis = 1000 * (60 * (long) mIntent.getIntExtra("minutes2", 0) + mIntent.getIntExtra("seconds2", 0) + 1);
+                        mResting = true;
+                        startTimer();
+                    }
+                    else{
+                        mTextTitle.setText("Done!");
+                        return;
+                    }
+                }
+                else{ // Finished Rest
+                    mTimeLeftInMillis = 1000 * (60 * (long) mIntent.getIntExtra("minutes", 0) + mIntent.getIntExtra("seconds", 0) + 1);
+                    mResting = false;
+                    if (setsSoFar < mNumSets){ // if true, do again
+                        setsSoFar++;
+                        startTimer();
+                    }
+                    else{ // done
+                        mTextTitle.setText("Done!");
+                        return;
+                    }
+                }
                 mButtonStartPause.setText("Start");
                 mButtonStartPause.setVisibility(View.INVISIBLE);
             }
@@ -82,7 +120,6 @@ public class PrepareActivity extends AppCompatActivity {
         mTimerRunning = true;
         mButtonStartPause.setText("pause");
         mButtonStartPause.setVisibility(View.VISIBLE);
-
     }
 
     private void pauseTimer() {
